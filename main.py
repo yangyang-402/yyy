d0a002d22e5aea752f7c830908f8fd37a44dd8aa
yangyang-402 <130953083+yangyang-402@users.noreply.github.com>
2023-04-17 15:08:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import random
+import fixieai
 
+BASE_PROMPT = "I am a simple agent that generates a random number between two given values."
+FEW_SHOTS = """
+Q: Generate a random number between 0 and 19.
+Ask Func[genrand]: 0, 19
+Func[genrand] says: 17
+A: The random number is 17.
+
+Q: Generate a random value from 5 to 10, inclusive.
+Ask Func[genrand]: 5, 10
+Func[genrand] says: 8
+A: The random number is 8.
+"""
+
+agent = fixieai.CodeShotAgent(BASE_PROMPT, FEW_SHOTS)
+
+@agent.register_func()
+def genrand(query: fixieai.Message) -> str:
+    low, high = query.text.replace(" ", "").split(",")
+    return str(random.randint(int(low), int(high)))
